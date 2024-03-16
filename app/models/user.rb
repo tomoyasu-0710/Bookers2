b4d3add71c309ccb32f3f44c13602054405d86ba
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-03-16 12:40:44 +0000

    [add]ヘッダー,フッター,aboutページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+   has_many :books, dependent: :destroy
+  
 end
