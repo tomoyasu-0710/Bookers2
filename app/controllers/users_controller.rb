433ac261a044f868311bddbccc33ff2864bdec71
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-03-16 07:30:58 +0000

    [add] controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  private
+  def user_params
+    params.require(:name).permit(:name, :profile_image, :introduction)
+  end
+  
 end
