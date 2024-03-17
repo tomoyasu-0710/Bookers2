b2afc144871539a3e11bfe724997848bb9dd9969
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-03-17 11:03:04 +0000

    [add] sign upページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
      before_action :configure_permitted_parameters, if: :devise_controller?
      
       def after_sign_in_path_for(resource)
-        users_id_path(resource)
+        users_path(resource)
+      end
+      
+      def after_sign_out_path_for(resource)
+        root_path
       end
 
   protected
