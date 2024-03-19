e57100035b686bb6fdedbf3a2fc54659aec2be97
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-03-19 13:45:35 +0000

    [add]log in book.index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,37 @@
 class BooksController < ApplicationController
+  def new
+     @book = Book.new
+  end
+
   def index
     @books = Book.all
-    @book = Book.new
   end
 
   def show
-    
-  end
-
-  def edit
-    
+    @book = Book.find(params[:id])
   end
   
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to books_path
+  end
+
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book_path
+  end
+  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
+  end
 end
