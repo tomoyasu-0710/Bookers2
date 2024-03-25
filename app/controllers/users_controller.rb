73f893917c1e76d223ad07e30380edd59ced7af7
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-03-25 12:45:16 +0000

    [add]各viewページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,13 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
 
   private
   def user_params
