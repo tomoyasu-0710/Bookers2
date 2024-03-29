b45620a54606a6c52635eb8c36beb5b8fefef1de
tomoyasu-0710 <157125081+tomoyasu-0710@users.noreply.github.com>
2024-03-29 09:11:18 +0000

    [fix]book edit アクセス制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -64,10 +64,10 @@ class BooksController < ApplicationController
    end
    
    def is_matching_login_user
-    user = User.find(params[:id])
-    unless user.id == current_user.id
+    book = Book.find(params[:id])
+    unless book.id == current_user.id
       redirect_to user_path(current_user.id)
     end
-  end
+   end
 
 end
